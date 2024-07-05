528e10f855186bd9edb85b6c932cd523b53558f4
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2024-07-05 18:22:06 +0530

    Done Basic setup of NextJs

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import {Button} from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <>
+    <p className='p-5'> This is admin Dashboard</p>
+    <div className='p-4'>
+        <Button>Click me</Button>
+    </div>
+    
+    </>
+  );
+}
