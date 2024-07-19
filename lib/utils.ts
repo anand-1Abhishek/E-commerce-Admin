d13d4ad797bea6746a4ac4bc3dd30c6d4f3aff69
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2024-07-19 10:44:21 +0530

    Added products entity and its api

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+//we have added formatter for product {currency}
+export const formatter = new Intl.NumberFormat("en-IN",{
+  style:'currency',
+  currency:'INR'
+})
