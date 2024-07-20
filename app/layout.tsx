4ec595ab7a6af35a0ca28241b6ce572062dda43d
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2024-07-20 12:58:29 +0530

    Added dark and light mode functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,35 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
-import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
 import { ToastProvider } from "@/providers/toast-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
-  description: "Admin Dashboard",
+  description: "Admin dashboard of our ecommerce store",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode; //datatype of children
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <ToastProvider/>
-          <ModalProvider/>
-          {children}
+            <ModalProvider/>
+                {children}
+        </ThemeProvider>
         </body>
-      </html>
+    </html>
     </ClerkProvider>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 import { MainNav } from "@/components/main-nav";        //it will be a destructured thing
 import StoreSwitcher from "@/components/store-switcher";
 import prismadb from "@/lib/prismadb";
-// import { ThemeToggle } from "@/components/theme-toggle";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 const Navbar = async () => {
     const { userId } = auth(); //user verification
@@ -19,13 +19,13 @@ const Navbar = async () => {
             userId,
         },
     });
-    // console.log(stores);
     return ( 
         <div className="border-b">
             <div className="flex h-16 items-center px-4">
                 <StoreSwitcher items={stores}/>
                 <MainNav className="mx-6"/>
                 <div className="ml-auto flex items-center space-x-4">
+                    <ThemeToggle/>
                     <UserButton afterSignOutUrl="/"/>  
                 </div>
             </div>
